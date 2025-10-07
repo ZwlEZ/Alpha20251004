7c40541db7e3d35bc644b661f9aeb39a09f56b63
SL Zwl <Zwl9090@outlook.com>
2025-10-07 16:23:28 +0800

    1

--- Alpha[[20251004]/main.c
+++ Alpha[[20251004]/main.c
@@ -1,11 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 
+//首
+void Z_A_A(void)
+{
+	printf("首\n");
+
+}
+
 int main()
 {
 	printf("欢迎使用。\n");
-	int a;
-	scanf("%d", &a);
-	
 	return 0;
 }
